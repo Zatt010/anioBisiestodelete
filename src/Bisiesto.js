90d57bf0d48f7890516f25478bd06bbe5128bea1
Zatt010 <afuheho1@gmail.com>
2023-08-18 16:18:57 -0400

    Implementacion regla ES anio bisiesto si es divisible entre 4 y no entre 100

--- src/Bisiesto.js
+++ src/Bisiesto.js
@@ -1,10 +1,7 @@
 function bisiesto (anio){
-  if (anio%100 == 0 && anio%400 == 0)
+  if (anio%100 != 0 && anio%4 == 0)
   {
     return true
-  }
-  else  if (anio%100 == 0 && anio%400 != 0){
-    return false
   }
   return false
 }
--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -1,11 +1,11 @@
 import bisiesto from "./Bisiesto.js";
 
 describe("Es bisiesto", () => {
-    it("Deberia ser bisiesto si es divisible entre 400 y entre 100", () => {
-      const result = bisiesto(2000)
+    it("Deberia ser bisiesto si es divisible entre 4 y no entre 100", () => {
+      const result = bisiesto(2008)
       expect(result).toEqual(true)
     });
-    it("no deberia ser bisiesto si es divisible entre 100 y no entre 400", () => {
+    it("no deberia ser bisiesto", () => {
         const result = bisiesto(1800 )
         expect(result).toEqual(false)
       });
